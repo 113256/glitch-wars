a80f765236c710ded5d08760d2d8078767e8a0ce
113256 <jackchan715@hotmail.com>
2015-08-14 11:07:40 -0700

    updated music manager

---
+++ Assets/Scripts/LevelManagerSplash.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagerSplash : MonoBehaviour {
+
+	//seconds to wait before loading next level
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	//pass number of levels
+	//e.g. if size = 2 there will be 2 indexes, each index corresponds to the level in build settings, index 0 = level0
+	//then you can drag a music clip to each element
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+	void Awake(){
+		//instructs music manager to persist
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//level = current level
+	//monobehaviour method
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];	
+
+		if (thisLevelMusic) {//if theres music attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
