3d092e8ac6485b7e4b7a91beef4b71b5177e71eb
jack chan <jackchan715@hotmail.com>
2015-09-03 19:03:47 -0700

    created button class

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	//pass a prefab to each button so that they spawn it
+	public GameObject defenderPrefab;
+
+	private Button[] buttonArray;
+	//static because we want to access it from other classes without declaring an object first
+	private static GameObject selectedDefender;//shared by all buttons
+
+	void Start(){
+		buttonArray = GameObject.FindObjectsOfType<Button> ();
+	}
+
+	void OnMouseDown(){
+		print ("button clicked");
+
+		//set all buttons to black then the one we've clicked to white
+		foreach (Button button in buttonArray) {
+			button.GetComponent<SpriteRenderer>().color = Color.black;
+		}
+		//set the one we've clicked to white
+		GetComponent<SpriteRenderer> ().color = Color.white;
+
+		selectedDefender = defenderPrefab;
+	}
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,9 +7,14 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectileParent;
 	public GameObject gun;
 
+	void Start(){
+		projectileParent = GameObject.Find("Projectiles");
+	}
+
 	private void Fire(){
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
+		//the "gun" child object is just there so that the projectile can spawn on top of it
 		newProjectile.transform.position = gun.transform.position;
 	}
 
