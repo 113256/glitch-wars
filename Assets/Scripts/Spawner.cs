b5be82835e1896c5ddec1a4e6ac711e724ba5def
jack chan <jackchan715@hotmail.com>
2015-09-04 15:36:51 -0700

    added spawner class

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public int spawnRate;
+	public GameObject[] attackerArray;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", 0.000001f, 2);
+	}
+
+	void Spawn(){
+		GameObject attacker = Instantiate (attackerArray [1], this.transform.position, Quaternion.identity) as GameObject;
+		attacker.transform.parent = this.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
